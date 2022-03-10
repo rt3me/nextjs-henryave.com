ca8e5f82bd3c4c2116a1a922171329d75aaba84d
Robert Turner <robturneris@gmail.com>
2022-03-09 23:48:36 -0500

    Updated site metadata

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Henry Avenue',
+  title: 'Henry Avenue',
+  description:
+    'Henry Avenue is a web development company focusing on React, Next.js, Node.js, MongoDB and JAMstack',
   locale: 'en',
 };
