06a55e9bf08f5e4307ef0f5543cbbb4fa5d10a71
Robert Turner <robturneris@gmail.com>
2022-03-10 23:41:10 -0500

    Added four sisters project

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -8,10 +8,11 @@ const VerticalFeatures = () => (
     id="portfolio"
   >
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature.svg"
-      imageAlt="First feature alt text"
+      title="Four Sisters Closet"
+      description="Four Sisters Closet is a women's clothing boutique operating exclusively online. They offer affordable clothing and accessories. Previously, the boutique shop used a free website provided by a credit card processor. The site was visually unappealing, performed slowly, and had poor SEO. Four Sisters Closet decided to ask Henry Avenue to build a new site on the Shopify platform."
+      link="https://foursisterscloset.com"
+      image="/assets/images/four-sisters-project-small.png"
+      imageAlt="Four Sisters Closet website"
     />
     <VerticalFeatureRow
       title="Your title here"
