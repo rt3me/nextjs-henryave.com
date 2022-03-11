489ba45d4d0bd0e16802d74a07693044743b2b56
Robert Turner <robturneris@gmail.com>
2022-03-10 23:38:15 -0500

    Added link to vertical feature type and text

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 type IVerticalFeatureRowProps = {
   title: string;
   description: string;
+  link?: string;
   image: string;
   imageAlt: string;
   reverse?: boolean;
@@ -24,12 +25,15 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
 
   return (
     <div className={verticalFeatureClass}>
-      <div className="w-full sm:w-1/2 text-center sm:px-6">
-        <h3 className="text-3xl text-gray-900 font-semibold">{props.title}</h3>
+      <div className="w-full text-center sm:w-1/2 sm:px-6">
+        <h3 className="text-3xl font-semibold text-gray-900">{props.title}</h3>
+        <h4>
+          <a href={props.link}>{props.link}</a>
+        </h4>
         <div className="mt-6 text-xl leading-9">{props.description}</div>
       </div>
 
-      <div className="w-full sm:w-1/2 p-6">
+      <div className="w-full p-6 sm:w-1/2">
         <img src={`${router.basePath}${props.image}`} alt={props.imageAlt} />
       </div>
     </div>
