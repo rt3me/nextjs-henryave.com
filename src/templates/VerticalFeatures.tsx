2effdc74c023a4b29e1ac37cc06047814804fcbc
Robert Turner <robturneris@gmail.com>
2022-03-13 23:13:31 -0400

    Updated features section description

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -4,23 +4,15 @@ import { Section } from '../layout/Section';
 const VerticalFeatures = () => (
   <Section
     title="Our work"
-    description="Take a look at a few of our favorite projects."
+    description="A few of our favorite projects."
     id="portfolio"
   >
-    <VerticalFeatureRow
-      title="Four Sisters Closet"
-      description="Four Sisters Closet is a women's clothing boutique operating exclusively online. They offer affordable clothing and accessories. Previously, the boutique shop used a free website provided by a credit card processor. The site was visually unappealing, performed slowly, and had poor SEO. Four Sisters Closet decided to ask Henry Avenue to build a new site on the Shopify platform."
-      link="https://foursisterscloset.com"
-      image="/assets/images/four-sisters-project-small.png"
-      imageAlt="Four Sisters Closet website"
-    />
     <VerticalFeatureRow
       title="Pamfa"
       description="Pamfa United Citizens is a designer brand that takes pride in creating clothes that allow a person to be truly expressive. With the decision to create a site that meets the Pamfa level of excellence, came the decision to work with Henry Avenue. This project resulted in an e-commerce site, powered by WordPress, WooCommerce, and Stripe."
       link="https://pamfa.com"
       image="/assets/images/pamfa-project-small.png"
       imageAlt="Pamfa website"
-      reverse
     />
     <VerticalFeatureRow
       title="A Notary On the Go"
@@ -28,6 +20,14 @@ const VerticalFeatures = () => (
       link="https://anotaryonthego.henryave.com"
       image="/assets/images/notary-project-small.png"
       imageAlt="A Notary On the Go website"
+      reverse
+    />
+    <VerticalFeatureRow
+      title="Four Sisters Closet"
+      description="Four Sisters Closet is a women's clothing boutique operating exclusively online. They offer affordable clothing and accessories. Previously, the boutique shop used a free website provided by a credit card processor. The site was visually unappealing, performed slowly, and had poor SEO. Four Sisters Closet decided to ask Henry Avenue to build a new site on the Shopify platform."
+      link="https://foursisterscloset.com"
+      image="/assets/images/four-sisters-project-small.png"
+      imageAlt="Four Sisters Closet website"
     />
   </Section>
 );
